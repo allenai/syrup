427be8beebfb91cbf8b36d9fb4ee1b86d9e1d8f2
Sam Skjonsberg <skoneberg@gmail.com>
2014-11-10 14:52:28 -0800

    If a file doesn't exist to serve statically, serve index.html instead.  This is a temporary solution.

--- server/server.js
+++ server/server.js
@@ -15,6 +15,13 @@ var app = express();
 app.use(griddle(base, watch));
 app.use(express.static(serve));
 
+// If a static file doesn't exist, resolve to index.
+// TODO(codeviking): Add an option for disabling / enabling this.
+// TODO(codeviking): How to throw 404s as is appropriate?
+app.use(function(req) {
+  res.end(fs.readFileSync(path.resolve(serve, 'index.html')).toString());
+});
+
 app.listen(port, function() {
   if(process && process.send) {
     process.send({ event: 'SERVER_STARTED', data: 'http://localhost:' + port });
