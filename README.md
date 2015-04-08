97ca39f335332c0f4aa2cca240ec1ec3a7208cc0
Sam Skjonsberg <skoneberg@gmail.com>
2015-04-08 09:03:38 -0700

    Disable stringify by default; Add option for disabling babel.

--- README.md
+++ README.md
@@ -14,30 +14,66 @@ npm install syrup
 
 ### syrup.gulp.init(gulp, [options, configParameters, paths])
 
-```
+```javascript
 /**
  * Registers default gulp tasks.
  *
  * @param {object}  gulp                                The gulp library.
- * @param {object}  [options]                           Optional object definining configuration parameters.
- * @param {boolean} [options.compressJs=true]           If true javascript will be minified. Defaults to true. This causes the build to become significantly slower.
- * @param {boolean} [options.sourceMaps=true]           Enables javascript source maps. Defaults to true.
- * @param {boolean} [options.compressCss=true]          If true styles will be compressed. Defaults to true.
- * @param {boolean} [options.detectGlobals=true]        Enables browserify global detection (and inclusion).  This is necessary for certain npm packages to work when bundled for front-end inclusion.  Defaults to true.  
- * @param {boolean} [options.insertGlobals=false]       Enables automatic insertion of node globals when preparing a javascript bundler.  Faster alternative to detectGlobals.  Causes an extra ~1000 lines to be added to the bundled javascript.  Defaults to false.
+ * @param {object}  [options]                           Optional object definining configuration
+ *                                                      parameters.
+ * @param {boolean} [options.compressJs=true]           If true javascript will be minified.
+ *                                                      Defaults to true. This causes the build
+ *                                                      to become significantly slower.
+ * @param {boolean} [options.sourceMaps=true]           Enables javascript source maps. Defaults
+ *                                                      to true.
+ * @param {boolean} [options.compressCss=true]          If true styles will be compressed.
+ *                                                      Defaults to true.
+ * @param {boolean} [options.detectGlobals=true]        Enables browserify global detection and
+ *                                                      inclusion.  This is necessary for certain
+ *                                                      npm packages to work when bundled for
+ *                                                      front-end inclusion.  Defaults to true.
+ * @param {boolean} [options.insertGlobals=false]       Enables automatic insertion of node
+ *                                                      globals when preparing a javascript
+ *                                                      bundler.  Faster alternative to
+ *                                                      detectGlobals.  Causes an extra ~1000
+ *                                                      lines to be added to the bundled
+ *                                                      javascript.  Defaults to false.
  * @param {boolean} [options.disableJsHint=false]       Disables jshint.  Defaults to false.
- * @param {boolean} [options.handleExceptions=false]    If an exception is encountered while compiling less or bundling javascript, capture the associated error and output it cleanly. Defaults to faulse.
- * @param {string}  [options.jsOut]                     Overrides the default filename for the resulting javascript bundle.  If not set the javascript file will be the same name as the entry point. 
- * @param {object}  [configParameters]                  Optional map of configuration keys. If set each key is searched for in the built HTML and replaced with the corresponding value.
- * @param {object}  [paths]                             Optional object defining paths relevant to the project. Any specified paths are merged with the defaults where these paths take precedence.
- * @param {string}  paths.base                          The base directory of your project where the gulpfile lives.  Defaults to the current processes working directory.
+ * @param {boolean} [options.handleExceptions=false]    If an exception is encountered while
+ *                                                      compiling less or bundling javascript,
+ *                                                      capture the associated error and output
+ *                                                      it cleanly. Defaults to false.
+ * @param {string}  [options.jsOut]                     Overrides the default filename for the
+ *                                                      resulting javascript bundle.  If not set
+ *                                                      the javascript file will be the same name
+ *                                                      as the entry point.
+ * @param {boolean} [options.disableBabel=false]        Optionally disable babel, the es6 to es6
+ *                                                      (and react JSX) transpiler.
+ *                                                      See http://babeljs.io for more information.
+ * @param {boolean} [options.enableStringify=false]     Optionally enable stringify, a browserify
+ *                                                      transform that allows HTML files to be
+ *                                                      included via require.
+ * @param {object}  [configParameters]                  Optional map of configuration keys. If
+ *                                                      set each key is searched for in the built
+ *                                                      HTML and replaced with the corresponding
+ *                                                      value.
+ * @param {object}  [paths]                             Optional object defining paths relevant
+ *                                                      to the project. Any specified paths are
+ *                                                      merged with the defaults where these paths
+ *                                                      take precedence.
+ * @param {string}  paths.base                          The base directory of your project where
+ *                                                      the gulpfile lives.  Defaults to the
+ *                                                      current processes working directory.
  * @param {string}  paths.html                          Path to the project's HTML files.
- * @param {string}  paths.jshint                        Path to the javascript files which should be linted using jshint.
+ * @param {string}  paths.jshint                        Path to the javascript files which should
+ *                                                      be linted using jshint.
  * @param {string}  paths.js                            Javascript entry point.
- * @param {string}  paths.allLess                       Path matching all less files which should be watched for changes.
+ * @param {string}  paths.allLess                       Path matching all less files which should
+ *                                                      be watched for changes.
  * @param {string}  paths.less                          The less entry-point.
  * @param {string}  paths.assets                        Path to the project's static assets.
- * @param {string}  paths.build                         Output directory where the build artifacts should be placed.
+ * @param {string}  paths.build                         Output directory where the build artifacts
+ *                                                      should be placed.
  *
  * @returns {undefined}
  */
--- gulp/index.js
+++ gulp/index.js
@@ -82,62 +82,61 @@ module.exports = {
    *
    * @param {object}  gulp                                The gulp library.
    * @param {object}  [options]                           Optional object definining configuration
-   *                                                		  parameters.
-   * @param {boolean} [options.compressJs=true]           If true javascript will be minified. Defaults
-   *                                                		  to true. This causes the build to become
-   *                                                			significantly slower.
-   * @param {boolean} [options.sourceMaps=true]           Enables javascript source maps. Defaults to
-   *                                                    	true.
-   * @param {boolean} [options.compressCss=true]          If true styles will be compressed. Defaults to
-   *                                                		  true.
-   * @param {boolean} [options.detectGlobals=true]        Enables browserify global detection (and
-   *                                                		  inclusion).  This is necessary for certain
-   *                                                			npm packages to work when bundled for front-end
-   *                                                			inclusion.  Defaults to true.  Enabling this
-   *                                                			may slow down your build.
-   * @param {boolean} [options.insertGlobals=false]       Enables automatic insertion of node globals
-   *                                                	   	when preparing a javascript bundler.  Faster
-   *                                                			alternative to detectGlobals but causes the
-   *                                                			javascript file to include an extra 1000 lines
-   *                                                			of nodejs globals with every build.  Defaults
-   *                                                			to false.
+   *                                                      parameters.
+   * @param {boolean} [options.compressJs=true]           If true javascript will be minified.
+   *                                                      Defaults to true. This causes the build
+   *                                                      to become significantly slower.
+   * @param {boolean} [options.sourceMaps=true]           Enables javascript source maps. Defaults
+   *                                                      to true.
+   * @param {boolean} [options.compressCss=true]          If true styles will be compressed.
+   *                                                      Defaults to true.
+   * @param {boolean} [options.detectGlobals=true]        Enables browserify global detection and
+   *                                                      inclusion.  This is necessary for certain
+   *                                                      npm packages to work when bundled for
+   *                                                      front-end inclusion.  Defaults to true.
+   * @param {boolean} [options.insertGlobals=false]       Enables automatic insertion of node
+   *                                                      globals when preparing a javascript
+   *                                                      bundler.  Faster alternative to
+   *                                                      detectGlobals.  Causes an extra ~1000
+   *                                                      lines to be added to the bundled
+   *                                                      javascript.  Defaults to false.
    * @param {boolean} [options.disableJsHint=false]       Disables jshint.  Defaults to false.
-   * @param {boolean} [options.handleExceptions=false]    If an exception is encountered while compiling
-   *                                                      less or bundling javascript, don't break the
-   *                                                      build -- just output the associatd error.
-   *                                                      Defaults to false.
+   * @param {boolean} [options.handleExceptions=false]    If an exception is encountered while
+   *                                                      compiling less or bundling javascript,
+   *                                                      capture the associated error and output
+   *                                                      it cleanly. Defaults to false.
    * @param {string}  [options.jsOut]                     Overrides the default filename for the
-   *                                                      bundled js file.  Defaults to the same
-   *                                                      filename as the js entry point (i.e. if
-   *                                                      paths.js is set to "app/app.js" the
-   *                                                      bundled file will be "build/app.js").
-   * @param {object}  [configParameters]                  Optional map of configuration keys. If set each
-   *                                                    	key is searched for in the html contents of the
-   *                                                     	application and replaced with the corresponding
+   *                                                      resulting javascript bundle.  If not set
+   *                                                      the javascript file will be the same name
+   *                                                      as the entry point.
+   * @param {boolean} [options.disableBabel=false]        Optionally disable babel, the es6 to es6
+   *                                                      (and react JSX) transpiler.
+   *                                                      See http://babeljs.io for more information.
+   * @param {boolean} [options.enableStringify=false]     Optionally enable stringify, a browserify
+   *                                                      transform that allows HTML files to be
+   *                                                      included via require.
+   * @param {object}  [configParameters]                  Optional map of configuration keys. If
+   *                                                      set each key is searched for in the built
+   *                                                      HTML and replaced with the corresponding
    *                                                      value.
-   * @param {object}  [paths]                             Optional object defining paths relevant to the
-   *                                                    	project. If not specified the defaults are used.
-   * @param {string}  paths.base                          The base directory of your project where the
-   *                                                    	gulpfile itself lives.  Defaults to the current
-   *                                                     	working directory.
-   * @param {string}  paths.html                          Path to the project's HTML files which should
-   *                                                    	be copied into the output directory.
-   * @param {string}  paths.jshint                        Path to the javascript files which should be
-   *                                                    	linted using jshint.
-   * @param {string}  paths.js                            Javascript entry point. It and all dependencies
-   *                                                    	loaded via require() will be bundled into
-   *                                                     	a single javascript file of the same name.
-   * @param {string}  paths.allLess                       Path to all less files which will be watched
-   *                                                      for changes and cause less re-compilation as
-   *                                                      changes occur.
-   * @param {string}  paths.less                          The less entry-point.  The less file and it's
-   *                                                      dependencies (specified using @import) will
-   *                                                      be compiled into a single static css file of
-   *                                                      the same name.
-   * @param {string}  paths.assets                        Path to the project's static assets (images,
-   *                                                      fonts, etc).
-   * @param {string}  paths.build                         Path to the project's build directory where the
-   *                                                      output should be placed.
+   * @param {object}  [paths]                             Optional object defining paths relevant
+   *                                                      to the project. Any specified paths are
+   *                                                      merged with the defaults where these paths
+   *                                                      take precedence.
+   * @param {string}  paths.base                          The base directory of your project where
+   *                                                      the gulpfile lives.  Defaults to the
+   *                                                      current processes working directory.
+   * @param {string}  paths.html                          Path to the project's HTML files.
+   * @param {string}  paths.jshint                        Path to the javascript files which should
+   *                                                      be linted using jshint.
+   * @param {string}  paths.js                            Javascript entry point.
+   * @param {string}  paths.allLess                       Path matching all less files which should
+   *                                                      be watched for changes.
+   * @param {string}  paths.less                          The less entry-point.
+   * @param {string}  paths.assets                        Path to the project's static assets.
+   * @param {string}  paths.build                         Output directory where the build artifacts
+   *                                                      should be placed.
+   *
    * @returns {undefined}
    */
   init: function(gulp, options, configParameters, paths) {
@@ -160,14 +159,14 @@ module.exports = {
       throw 'Invalid paths';
     }
 
-    // Support --builddir or --target as an override for the build directory.
+    // Support the --builddir or --target options as an override for the build directory.
     // TODO: Document this.  Should also provide the ability to pass all options and/or paths
     // via command flags, ie. gulp --paths.build=../public build
     if (gutil.env.builddir || gutil.env.target) {
       paths.build = gutil.env.builddir || gutil.env.target;
     }
 
-    // Finally, if NODE_BUILD_DIR is set, override build yet
+    // Finally, if NODE_BUILD_DIR is set, override paths.build
     // TODO: Document!
     if (process.env.NODE_BUILD_DIR) {
       paths.build = process.env.NODE_BUILD_DIR;
@@ -197,8 +196,12 @@ module.exports = {
         } else {
           bundlerInstance = b;
         }
-        bundlerInstance.transform(stringify({ extensions: ['.html'], minify: true }));
-        bundlerInstance.transform(babelify);
+        if (options.enableStringify) {
+          bundlerInstance.transform(stringify({ extensions: ['.html'], minify: true }));
+        }
+        if (!options.disableBabel) {
+          bundlerInstance.transform(babelify);
+        }
         // Browserify can't handle purely relative paths, so resolve the path for them...
         bundlerInstance.add(path.resolve(paths.base, paths.js));
         bundlerInstance.on('error', gutil.log.bind(gutil, 'Browserify Error'));
